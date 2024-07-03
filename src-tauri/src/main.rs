4d1926922d849fca1a3f58275c0c9e2b534fba95
Ishan Morwal <imorwal@proton.me>
2024-07-03 08:42:03 +0530

    main menu functionality

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,9 +6,27 @@ fn greet(name:&str) -> String{
   format!("Hello {name}")
 }
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
 fn main() {
+  let close = CustomMenuItem::new("close".to_string(), "Close");
+  let submenu = Submenu::new("File", Menu::new().add_item(close));
+  let menu = Menu::new()
+      .add_native_item(MenuItem::Copy)
+      .add_submenu(submenu);
+
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![greet])
+    .menu(menu)
+    .on_menu_event(|event| {
+        match event.menu_item_id() {
+            "close" => {
+                std::process::exit(0);
+            }
+            _ => {}
+        }
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,19 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 <script lang="ts">
-	
+import { appWindow } from '@tauri-apps/api/window';
+async function closeApp() {
+    try {
+      await appWindow.close();
+    } catch (error) {
+      console.error("Failed to close the app:", error);
+    }
+  }
 </script>
 <div class="container h-full mx-auto flex flex-col p-[256px] gap-5 justify-center items-center">
 	<header class='text-8xl'>Smriti</header>
-	
 	<div class="container mx-auto flex flex-col text-xl items-center justify-center">
 		<span class="btn text-xl"><a href='/write'>Write a note</a></span>
 		<span class="btn text-xl"><a href='/open'>Open a note</a></span>
+		<button class="btn text-xl" on:click={closeApp()}>Exit</button>
 	</div>
 </div>
